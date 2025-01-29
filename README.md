cb9f34e8c0e992a4a147da7b73d7ae78f340e6b8
Tom Collins <tom@tomlogic.com>
2025-01-29 12:47:05 -0500

    patch: release v1.04

--- README.md
+++ README.md
@@ -7,37 +7,46 @@ machine.  The initial version of the patch changes scoring in multiplayer
 games to match that of single-player games.
 
 If someone completes a race in a multiplayer game, the original ROM would
-reset all other players back to checkpoint 1, and give them credit for
-finishing the qualifying heat.  As a result, all checkpoint hurry-ups for
-those players are at the maximum of 800,000 points.
+reset all other players back to checkpoint 1, give them credit for finishing
+the qualifying heat, and enable the hurry-up at the start of their next ball.
+As a result, all checkpoint hurry-ups for those players are at the maximum
+of 800,000 points.
 
-The patch generates ROM files so those events (reset to checkpoint 1, credit
-for finishing the qualifying heat) only affect the current player.
+The patch generates ROM files so those events only affect the current player.
 
-## Using the Script
+In addition, starting with v1.03 of the patch, it improves RACE BONUS scoring
+(end of ball and lit RACE BONUS shots) and awards a multiplied 500K bonus at
+the end of each race.
+
+## Creating patched ROMs
 
 Because of Gottlieb's copyright enforcement, this repository doesn't include
 ROM images.  You'll need to dump your existing ROMs, or obtain dumps
 elsewhere.
 
+I recommend keeping the original PROMs for backup.  For EEPROM replacements,
+you can use AT28C64-15PC for PROM1 (8KB) and AT28C16-15PC for PROM2 (2KB).
+
+### IPS Patch Files
+If you're familiar with using IPS files for patching, you can use
+`victory-v1.XX-PROM1.ips` and `victory-v1.XX-PROM2.ips`.
+
+### Python Script
 Place `PROM1.CPU` and `PROM2.CPU` in the same directory as the `patch.py`
 script.  Reference the table below for file sizes/checksums.  Run `patch.py`
 with Python 3.  It will perform a simple checksum on the files before
-applying the patches to create `victory-v1.01-PROM1.bin` and
-`victory-v1.01-PROM2.bin`.
-
-The repository also includes IPS-formatted patch files as an alternate method
-of patching.
+applying the patches to create `victory-v1.XX-PROM1.bin` and
+`victory-v1.XX-PROM2.bin`.
 
 ## ROM checksums
 
-| Setting           | Original  | v1.01      |
+| Setting           | Original  | v1.04      |
 |-------------------|-----------|------------|
 | PROM1 file size   | 8192      | 8192       |
-| PROM1 checksum    | 0xD02F2   | 0xD03B4    |
-| PROM1 CRC32       | e724db90  | 3d673442   |
+| PROM1 checksum    | 0xD02F2   | 0xD0316    |
+| PROM1 CRC32       | e724db90  | 290889ce   |
 | PROM2 file size   | 2048      | 2048       |
-| PROM2 checksum    | 0x32B32   | 0x32B4E    |
-| PROM2 CRC32       | 6a42eaf4  | e5e1717c   |
-| enter self test   | TEST MODE | VCTRY 1,01 |
-| in-game check-sum | 02F2      | 03B4       |
+| PROM2 checksum    | 0x32B32   | 0x329DF    |
+| PROM2 CRC32       | 6a42eaf4  | aec1995f   |
+| enter self test   | TEST MODE | VCTRY 1,04 |
+| in-game check-sum | 02F2      | 0316       |
--- patch.py
+++ patch.py
@@ -11,20 +11,50 @@ Released under GPL-3.0 License
 
 Version History
 ---------------
-2024-12-23 v1.01 Initial release.  Only reset current player to checkpoint 1
-                 and give credit for finishing Qualifying Heat.
+v1.01, Released 2024-12-23
+    - Initial release.
+    - Only reset current player to checkpoint 1 and give credit for finishing
+      Qualifying Heat.
+
+v1.02, unreleased
+    - Upon race completion, don't modify flag used to enable checkpoint
+      hurry-up at start of ball.
+    - Track count of completed checkpoints, and base bonus on that value
+      instead of just the current checkpoint number.
+    - Race Bonus:
+      was 10K x checkpoint number
+      now 50K x (completed checkpoints + 1)
+          (the same as 50K x (completed races x 8 + checkpoint number))
+
+v1.03, custom INDISC 2025 version, released 2025-01-19
+    - Score multiplied 500K upon race completion (in addition to hurry-up,
+      if any).
+
+v1.04, public version of v1.03, released 2025-01-31
+    - No functional changes.
 """
 
+from ips_util.ips_util import Patch
+import os
 
 # Values for files created by this script.
-VERSION = '1.01'
-PROM1_CHECKSUM = 0xD03B4
-PROM2_CHECKSUM = 0x32B4E
+VERSION = '1.04'
+CHECKSUMS = [0xD0316, 0x329DF]
+
+# shortcuts for opcodes with unique mnemonics (e.g., not LDA which has multiple versions)
+NOP = b'\xEA'       # this notation allows for `NOP * 20` for 20 NOP opcodes
+JSR = 0x20
+RTS = 0x60
+TYA = 0x98
+TAY = 0xA8
+
+# reference binary files in our directory
+my_dir = os.path.dirname(__file__)
 
 
 def load(file_in, expected_checksum):
     try:
-        with open(file_in, 'rb') as f:
+        with open(os.path.join(my_dir, file_in), 'rb') as f:
             source_bytes = bytearray(f.read())
     except FileNotFoundError:
         print('Error: Script requires original PROM1.CPU and PROM2.CPU files.')
@@ -38,39 +68,35 @@ def load(file_in, expected_checksum):
     return source_bytes
 
     
-def save(file_out, data):
-    with open(file_out, 'wb') as f:
+def save(file_out, data, expected_checksum):
+    with open(os.path.join(my_dir, file_out), 'wb') as f:
         f.write(data)
+
+    actual_checksum = checksum(data)
+    if actual_checksum == expected_checksum:
+        print("Success: %s checksum is expected 0x%X" % (file_out, actual_checksum))
+    else:
+        print("Warning: %s file checksum 0x%X != 0x%X" %
+              (file_out, actual_checksum, expected_checksum))
 
 
 def patch(data, offset, new_bytes):
-    for b in new_bytes:
-        data[offset] = b
-        offset += 1
-        
-
-def patch_prom1(CHECKSUM_P2):
-    p1 = load('PROM1.CPU', 0xD02F2)
-    
-    # replace "TEST MODE" with "VCTRY 1,01" (\xB1 is 1 followed by comma)
-    (major, minor) = VERSION.split('.')
-    patch(p1, 0x00E8, b"VCTRY %c%s\xFF" % ((0x80 | ord(major)), str.encode(minor)))
-    
-    # update checksum of P2 and reset P1 checksum to 0xFFFF
-    patch(p1, 0x1FF6, [(CHECKSUM_P2 >> 8) & 0xFF, CHECKSUM_P2 & 0xFF, 0xFF, 0xFF])
-
-    # calculate new checksum, with 0xFFFF in that position
-    stored_checksum = checksum(p1)
-    patch(p1, 0x1FF8, [(stored_checksum >> 8) & 0xFF, stored_checksum & 0xFF])
-    save('victory-v%s-PROM1.bin' % VERSION, p1)
-
-    # re-calculate file's actual checksum
-    actual_checksum = checksum(p1)
-    if actual_checksum == PROM1_CHECKSUM:
-        print("Success: PROM1 file checksum is expected 0x%X" % actual_checksum)
+    length = 0
+    if isinstance(new_bytes, list):
+        for item in new_bytes:
+            length += patch(data, offset + length, item)
+    elif isinstance(new_bytes, bytes):
+        for b in new_bytes:
+            data[offset + length] = b
+            length += 1
+    elif isinstance(new_bytes, int):
+        while new_bytes or length == 0:
+            data[offset + length] = new_bytes & 0xFF
+            length += 1
+            new_bytes >>= 8
     else:
-        print("Warning: modified PROM1 file checksum 0x%X != 0x%X" %
-              (actual_checksum, PROM1_CHECKSUM))
+        raise ValueError('unknown type passed to patch(): ' + type(new_bytes))
+    return length
 
 
 def checksum(data):
@@ -80,53 +106,228 @@ def checksum(data):
     return check
     
     
-def patch_prom2():
-    p2 = load('PROM2.CPU', 0x32B32)
-
-    # only reset current player to checkpoint 0
+def patch_rom(rom):
+    # Get rid of all of this code from completing a race.  One player's action shouldn't
+    # impact other players' games.
     """
-    12FD				.reset_player_checkpoints_to_0:
-    12FD : A9 00		"  "		lda	#$00
-    12FF : 85 ED		"  "		sta	var_player_checkpoint[]
-    1301 : 85 EE		"  "		sta	X00EE
-    1303 : 85 EF		"  "		sta	X00EF
-    1305 : 85 F0		"  "		sta	X00F0
-
-    Replace with:
-                        .reset_current_player_checkpoint_to_0:
-    12FD : 20 91 33		"  3"		jsr	load_player_num_to_X
-    1300 : A9 00		"  "		lda	#$00
-    1302 : 95 ED		"  "		sta	var_player_checkpoint[],x
-    1304 : EA		    " "		    nop
-    1305 : EA		    " "		    nop
-    1306 : EA		    " "		    nop
+    12FD				.reset_all_player_checkpoints:
+    12FD : A9 00        "  "        lda     #$00         ; CHECKPOINT 1
+    12FF : 85 ED        "  "        sta     var_player_checkpoint[]
+    1301 : 85 EE        "  "        sta     X00EE
+    1303 : 85 EF        "  "        sta     X00EF
+    1305 : 85 F0        "  "        sta     X00F0
+    1307                .clear_all_player_F16@1307:
+    1307 : A9 10        "  "        lda     #$10         ; F16 [if set, no checkpoint bonus on next plunge] clear
+    1309 : 20 08 30     "  0"       jsr     func_update_player_lamp_A_for_all
+    130C                .set_curr_player_F16@130C:
+    130C : A9 90        "  "        lda     #$90         ; F16 [if set, no checkpoint bonus on next plunge] set
+    130E : 20 CF 2F     "  /"       jsr     func_update_player_lamp
     """
-    patch(p2, 0x2FD, b"\x20\x91\x33\xA9\x00\x95\xED\xEA\xEA\xEA")
+    reset_current_player_checkpoint = 0x12FD
+    patch(rom, reset_current_player_checkpoint, [
+        # this 15-byte sequence adds a multiplied, 500K bonus for completing the race
+        0xA9, 0x88,             # lda #$88      ; enable multiplied score
+        JSR, 0x2FCF,            # jsr func_update_player_flag
+
+        0xA9, 0x15,             # lda #$15         ; 500,000 points
+        JSR, 0x11A6,            # jsr func_add_multiplied_to_score
+
+        0xA9, 0x08,             # lda #$08      ; disable multiplied score
+        JSR, 0x2FCF,            # func_update_player_flag
+
+        NOP * 5                 # room for even more code!
+    ])
+
+    # replace unused func_update_X_player_flags_from_Y_for_all with a new function
+    func_load_player_checkpoint = 0x302E
+    patch(rom, func_load_player_checkpoint, [
+        JSR, 0x3391,            # jsr load_player_num_to_X
+        0xA9, 0x07,             # lda #$07
+        0x35, 0xED,             # and var_player_checkpoint[], x
+        TAY,                    # tay
+        RTS                     # rts
+    ])
+
+    """
+    ; replace this section of code
+    12C7 : 20 91 33         "  3"           jsr     load_player_num_to_X
+    12CA : B4 ED            "  "            ldy     var_player_checkpoint[],x
+    12CC : C0 07            "  "            cpy     #$07         ; CHECKPOINT 8
+    12CE : F0 16            "  "            beq     .at_last_checkpoint
+    12D0                            .advance_to_next_checkpoint:
+    12D0 : C8               " "             iny
+    12D1 : 94 ED            "  "            sty     var_player_checkpoint[],x
+    12D3 : 88               " "             dey
+    """
+    patch(rom, 0x12C7, [
+        JSR, func_load_player_checkpoint,   # jsr func_load_player_checkpoint
+        0xF6, 0xED,                         # inc var_player_checkpoint[],x
+    ])
+    # leave existing code at 0x12CC and 0x12D0
+    patch(rom, 0x12D0, [NOP, NOP, NOP, NOP])
 
     # only give current player credit for finishing qualifying heat
     """
     132F				.player_finished_qualifying_heat:
-    132F : A9 8E		"  "		lda	#$8E
-    1331 : 20 08 30		"  0"		jsr	func_update_player_lamps_for_all
+    132F : A9 8E		"  "		lda	#$8E         ; F14 [completed qualifying heat] set
+    1331 : 20 08 30		"  0"		jsr	func_update_player_flag_for_all
 
     132F				.player_finished_qualifying_heat:
-    132F : A9 8E		"  "		lda	#$8E
-    1331 : 20 CF 2F		"  /"		jsr	func_update_player_lamp
+    132F : A9 8E		"  "		lda	#$8E         ; F14 [completed qualifying heat] set
+    1331 : 20 CF 2F		"  /"		jsr	func_update_player_flag
     """
-    patch(p2, 0x332, b"\xCF\x2F")
-    
-    save('victory-v%s-PROM2.bin' % VERSION, p2)
-    actual_checksum = checksum(p2)
+    patch(rom, 0x1332, 0x2FCF)    # change function called from jsr
 
-    if actual_checksum == PROM2_CHECKSUM:
-        print("Success: PROM2 file checksum is expected 0x%X" % actual_checksum)
-    else:
-        print("Warning: modified PROM2 file checksum 0x%X != 0x%X" %
-              (actual_checksum, PROM2_CHECKSUM))
+    # replace code that loads the player's checkpoint with a JSR to this new code
+    #   12C7 : 20 91 33         "  3"           jsr     load_player_num_to_X
+    #   12CA : B4 ED            "  "            ldy     var_player_checkpoint[],x
 
-    return actual_checksum
+    for address in [0x1712, 0x174E, 0x1759, 0x3BFA]:
+        patch(rom, address, [
+            NOP,                                    # nop
+            JSR, func_load_player_checkpoint,       # jsr func_load_player_checkpoint
+            NOP                                     # nop
+        ])
+
+    """
+    Update func_is_player_at_checkpoint_Y, but first simplifying previous routine
+    (.mult_at_max_8X_so_score_mult_100K@1254) to jump to existing code (freeing up
+    some extra bytes).
+    """
+    """
+    Reduce code used for this routine, to free up two more bytes for
+    func_is_player_at_checkpoint_Y.
+        1254                            .mult_at_max_8X_so_score_mult_100K@1254:
+        1254 : A9 11            "  "            lda     #$11         ; 100,000 points
+        1256 : 4C A6 11         "L  "           jmp     func_add_multiplied_to_score
+    """
+    patch_length = patch(rom, 0x1254, [
+        # .mult_at_max_8X_so_score_mult_100K@1254:
+        0x4C, 0x114A,       # jmp .add_100K_multiplied_to_score (either 114A or 3EFA)
+    ])
+
+    """
+    Old code:
+        1259                            func_is_player_at_checkpoint_Y:
+        1259 : 20 91 33         "  3"           jsr     load_player_num_to_X
+        125C : 98               " "             tya
+        125D : D5 ED            "  "            cmp     var_player_checkpoint[],x
+
+    New code (with new entry address):
+        1257                            func_is_player_at_checkpoint_Y:
+        1257 : 20 91 33         "  3"           jsr     load_player_num_to_X
+        125A : 98               " "             tya
+        125B : 55 ED            "U "            eor     var_player_checkpoint[],x
+        125D : 29 07            ") "            and     #$07
+    """
+    player_at_checkpoint = 0x1254 + patch_length
+    patch(rom, player_at_checkpoint, [
+        # func_is_player_at_checkpoint_Y (new location)
+        JSR, 0x3391,        # jsr load_player_num_to_X        ; 3 X = player number
+        TYA,                # tya                             ; 1 A = checkpoint_test
+        0x55, 0xED,         # eor var_player_checkpoint[],x   ; 2 A = checkpoint_test ^ checkpoint_var
+        0x29, 0x07,         # and #$07                        ; 2 A = bottom three bits of that
+    ])
+
+    # fix all func_is_player_at_checkpoint_Y calls to use the new address
+    for address in [0x1032, 0x1085, 0x10A8, 0x3EC8, 0x3F1B, 0x3F54, 0x3F8B]:
+        patch(rom, address, [JSR, player_at_checkpoint])
+
+    # **** Big patch to award_bonus() to consider full checkpoint count.
+
+    # start by inserting func_toggle_bonus_lamp() before award_bonus()
+    func_toggle_bonus_lamp = 0x1416
+    patch_size = patch(rom, func_toggle_bonus_lamp, [
+        0xA5, 0xE7,         # lda X00E7
+        0x29, 0x07,         # and #$07
+        0xA8,               # tay
+        0xB9, 0x242D,       # lda tbl_checkpoint_lamps,y
+        0x09, 0x40,         # ora #$40
+        0x4C, 0x2F94        # jmp update_single_lamp_solenoid_from_A  ; chain into this function
+    ])
+    func_award_bonus = func_toggle_bonus_lamp + patch_size
+
+    # now build new award_bonus() after the new function
+    patch(rom, func_award_bonus, [
+        JSR, 0x15D6,        # jsr     func_set_var_E4_to_0
+        0xA9, 0x88,         # lda     #$88         ; F8 [Multiply Score] set
+        JSR, 0x2FCF,        # jsr     func_update_player_flag
+        0xA0, 0x11,         # ldy     #$11         ; L17 [CHECKPOINT #1 CAR & TARGET (2)] clear
+        0xA2, 0x08,         # ldx     #$08
+        JSR, 0x2FC6,        # jsr     update_X_lamps_solenoids_from_Y
+        JSR, 0x3391,        # jsr     load_player_num_to_X
+        0xB5, 0xED,         # lda     var_player_checkpoint[],x
+        0x85, 0xE7,         # sta     X00E7
+        JSR, func_toggle_bonus_lamp,      # jsr func_toggle_bonus_lamp
+        0xA9, 0xEB,         # lda     #$EB         ; sound(0xEB)
+        JSR, 0x2F64,        # jsr     func_play_sound_A
+        0xA9, 0x8E,         # lda     #$8E         ; L14 [CENTER LAMPS: TOP RIGHT/BOTTOM LEFT (2)] set
+        JSR, 0x2F94,        # jsr     update_single_lamp_solenoid_from_A
+        0xA9, 0x90,         # lda     #$90         ; L16 [CENTER LAMPS: TOP LEFT/BOTTOM RIGHT (2)] set
+        JSR, 0x2F94,        # jsr     update_single_lamp_solenoid_from_A
+        0xA9, 0x25,         # lda     #$25         ; 50,000 points
+        JSR, 0x11A6,        # jsr     func_add_multiplied_to_score
+        0xA9, 0x10,         # lda     #$10
+        JSR, 0x2F51,        # jsr     func_delay
+        JSR, func_toggle_bonus_lamp,  # jsr func_toggle_bonus_lamp
+        NOP,                # nop     ; re-align with original code
+    ])
+
+    # fix the bpl to the top of the bonus burndown loop
+    patch(rom, 0x1467, [0x10, 0xD0])
+
+    # fix all func_award_bonus calls to use the new address
+    for address in [0x3CA6, 0x3F07, 0x3F47]:
+        patch(rom, address, [JSR, func_award_bonus])
+
+    # **** end of patches to award_bonus()
+
+    # replace "TEST MODE" with "VCTRY 1,xx"
+    (major, minor) = VERSION.split('.')
+    patch(rom, 0x20E8, [
+        b"VCTRY ",
+        0x80 | ord(major),                  # set top bit for comma
+        str.encode(minor.replace('0', 'O')),
+        0xFF
+    ])
+
+    # update PROM2 and PROM1 checksums stored on PROM1
+
+    p2_checksum = checksum(rom[0x1000:0x1800])
+
+    # update checksum of PROM2 (in PROM1) and reset PROM1 checksum to 0xFFFF
+    patch(rom, 0x3FF6, [
+        (p2_checksum >> 8) & 0xFF, p2_checksum & 0xFF,          # reversed byte order!
+        0xFFFF
+    ])
+
+    # calculate new PROM1 checksum, with 0xFFFF in that position
+    stored_checksum = checksum(rom[0x2000:0x4000])
+    patch(rom, 0x3FF8, [
+        (stored_checksum >> 8) & 0xFF, stored_checksum & 0xFF   # reversed byte order!
+    ])
 
 
-# patch PROM2 first because PROM1 holds a PROM2 checksum    
-CHECKSUM_P2 = patch_prom2()
-patch_prom1(CHECKSUM_P2)
+# create an all 0xFF 16KB memory space to load ROMs into for patching
+ROM = bytearray([0xFF] * 4 * 4096)
+prom = [
+    load('PROM1.CPU', 0xD02F2),
+    load('PROM2.CPU', 0x32B32)
+]
+ROM[0x2000:0x4000] = prom[0]
+ROM[0x1000:0x1800] = prom[1]
+
+patch_rom(ROM)
+
+prom_patched = [
+    ROM[0x2000:0x4000],
+    ROM[0x1000:0x1800]
+]
+
+for i in range(0, 2):
+    basename = 'victory-v%s-PROM%u' % (VERSION, i + 1)
+    save('%s.bin' % basename, prom_patched[i], CHECKSUMS[i])
+    # Generate IPS file
+    patch = Patch.create(prom[i], prom_patched[i])
+    with open(os.path.join(my_dir, '%s.ips' % basename), 'wb') as ips:
+        ips.write(patch.encode())
